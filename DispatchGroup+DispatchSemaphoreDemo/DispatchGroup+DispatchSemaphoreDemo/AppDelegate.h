e8b92d1f979a1e190e7599e8e36c235769312bc9
你见唔到我 <403482009@qq.com>
2019-08-08 14:38:12 +0800

    DispatchGroup+DispatchSemaphoreDemo

---
+++ DispatchGroup+DispatchSemaphoreDemo/DispatchGroup+DispatchSemaphoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DispatchGroup+DispatchSemaphoreDemo
+//
+//  Created by Sylar on 2019/8/8.
+//  Copyright © 2019 jin10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DispatchGroup+DispatchSemaphoreDemo/DispatchGroup+DispatchSemaphoreDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DispatchGroup+DispatchSemaphoreDemo
+//
+//  Created by Sylar on 2019/8/8.
+//  Copyright © 2019 jin10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
